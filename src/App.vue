22be13f283ef039c7e2a26f554b2cd552f3e02f0
wookie227 <andrey-rychag23@mail.ru>
2025-02-13 15:11:35 +0700

    init commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Список проектов</h1>
+    <ProjectTable />
+  </div>
+</template>
+
+<script setup>
+import ProjectTable from './components/ProjectTable.vue';
+</script>
